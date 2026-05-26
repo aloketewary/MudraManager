b7360a4900c7af4ec731b8fb92ddbf350f9904f2
Aloke Tewary <aloke.tewary@here.com>
2026-05-26 18:41:19 +0530

    (bugfix) build failure fix

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -110,12 +110,3 @@ dependencies {
     testImplementation("org.json:json:20231013")
 }
 
-
-configurations.all {
-    resolutionStrategy {
-        // home_widget uses "1.+" for glance which resolves to 1.3.0-alpha01
-        // requiring compileSdk 37 + AGP 9.1.0. Pin to last compatible version.
-        force("androidx.glance:glance-appwidget:1.1.1")
-        force("androidx.glance:glance:1.1.1")
-    }
-}
--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -37,6 +37,14 @@ rootProject.layout.buildDirectory.value(newBuildDir)
 subprojects {
     buildDir = file("${rootProject.buildDir}/${project.name}")
     evaluationDependsOn(":app")
+    configurations.all {
+        resolutionStrategy {
+            // home_widget uses "1.+" for glance which resolves to 1.3.0-alpha01
+            // requiring compileSdk 37 + AGP 9.1.0. Pin to last compatible version.
+            force("androidx.glance:glance-appwidget:1.1.1")
+            force("androidx.glance:glance:1.1.1")
+        }
+    }
 }
 
 tasks.register<Delete>("clean") {
